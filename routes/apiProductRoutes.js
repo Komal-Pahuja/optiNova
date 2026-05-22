3748de52ab56d19b29d6752e3480fc3989e7bfe0
harleenkaur1215 <157448095+harleenkaur1215@users.noreply.github.com>
2026-05-22 09:19:31 +0530

    harleen commit

--- routes/apiProductRoutes.js
+++ routes/apiProductRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { apiGetProducts, apiGetProduct } = require('../controllers/productController');
+const { apiGetProducts, apiGetProduct, apiGetProductFilters, apiGetCategories } = require('../controllers/productController');
+const { apiGetRatings, apiCreateOrUpdateReview } = require('../controllers/reviewController');
+const { apiRequireAuth } = require('../middleware/auth');
 
 const router = express.Router();
 
+router.get('/filters', apiGetProductFilters);
+router.get('/categories', apiGetCategories);
+router.get('/ratings', apiGetRatings);
 router.get('/', apiGetProducts);
 router.get('/:id', apiGetProduct);
+router.post('/:id/rate', apiRequireAuth, apiCreateOrUpdateReview);
 
 module.exports = router;
